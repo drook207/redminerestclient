2de0b5658d3ae7af1f22366a5154b259c45e8885
Pascal Berndt <drook207@gmail.com>
2025-12-22 23:02:58 +0100

    Start implementation of client

--- src/restclient.cpp
+++ src/restclient.cpp
@@ -1,3 +1,42 @@
 #include "restclient.hpp"
 
-RestClient::RestClient() {}
+#include <qcontainerfwd.h>
+#include <qlogging.h>
+#include <qloggingcategory.h>
+#include <QLoggingCategory>
+#include <qnetworkaccessmanager.h>
+#include <qobject.h>
+#include <qrestaccessmanager.h>
+#include <qurl.h>
+
+Q_LOGGING_CATEGORY(lcRestClient, "redmine.restclient")
+namespace redmine {
+
+RestClient::RestClient(QObject* parent)
+    : m_Manager(new QRestAccessManager(new QNetworkAccessManager(this), this)), QObject(parent) {}
+
+void RestClient::setBaseUrl(const QUrl& newUrl) {
+    if (newUrl == m_BaseUrl) {
+        return;
+    }
+    m_BaseUrl = newUrl;
+    emit baseUrlChanged();
+}
+
+void RestClient::setApiKey(const QString& newApiKey) {
+    if (newApiKey == m_ApiKey) {
+        return;
+    }
+    m_ApiKey = newApiKey;
+    emit apiKeyChanged();
+}
+
+bool RestClient::connect() {
+    if (nullptr == m_Manager) {
+        qWarning(lcRestClient()) << tr("No valid instance for QNetworkAccessManager, abort!");
+        return false;
+    }
+    return true;
+}
+
+} // namespace redmine
--- src/restclient.hpp
+++ src/restclient.hpp
@@ -2,9 +2,14 @@
 #define RESTCLIENT_HPP
 
 #include <qhashfunctions.h>
+#include <qnetworkaccessmanager.h>
 #include <QNetworkAccessManager>
 #include <QObject>
+#include <QRestAccessManager>
 #include <qtclasshelpermacros.h>
+#include <qtmetamacros.h>
+#include <qtpreprocessorsupport.h>
+#include <qurl.h>
 
 #include "redminerestclient_global.h"
 namespace redmine {
@@ -20,10 +25,31 @@ class REDMINERESTCLIENT_EXPORT RestClient : public QObject {
     void setApiKey(const QString& newApiKey);
     [[nodiscard]] QString apiKey() const;
 
+    bool connect();
+    bool connect(const QUrl& baseUrl, const QString& apiKey);
+  signals:
+    void baseUrlChanged();
+    void apiKeyChanged();
+
+  private slots:
+    void finished();
+    static void authenticationRequired(QNetworkReply* /*reply*/, QAuthenticator* /*authenticator*/) {
+        Q_UNIMPLEMENTED();
+    };
+    static void encrypted(QNetworkReply* /*reply*/) { Q_UNIMPLEMENTED(); };
+    static void preSharedKeyAuthenticationRequired(QNetworkReply* /*reply*/,
+                                                   QSslPreSharedKeyAuthenticator* /*authenticator*/) {
+        Q_UNIMPLEMENTED();
+    };
+    static void proxyAuthenticationRequired(const QNetworkProxy& /*proxy*/, QAuthenticator* /*authenticator*/) {
+        Q_UNIMPLEMENTED();
+    };
+    void sslErrors(QNetworkReply* reply, const QList<QSslError>& errors);
+
   private:
     QUrl m_BaseUrl;
     QString m_ApiKey;
-    QNetworkAccessManager* m_Manager;
+    QRestAccessManager* m_Manager;
 };
 } // namespace redmine
 #endif // RESTCLIENT_HPP
